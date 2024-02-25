9f0489adca8d9472220d535cc9ce6c9a18d4c718
kkauy <anthonykkauy@gmail.com>
2024-02-24 22:58:29 -0800

    tenth git update

---
+++ Day3-intro JS/data-types.js
@@ -0,0 +1,7 @@
+//Numbers
+let score = 58;
+console.log("Type of score", score);
+
+//String
+let playName = "John";
+console.log("Type of playName", typeof playName);
---
+++ Day3-intro JS/variable.js
@@ -0,0 +1,15 @@
+//variable 
+
+if (true) {
+    var name = "John";
+  
+}
+  console.log(name);
+
+  // let variable
+  let age  = 18;
+
+  if(true) {
+    var animal = "cat";
+  }
+   console.log(animal); // ReferenceError: animal is not defined
