5a3aab5852d594d5454a55cab244b4645c31f997
kkauy <anthonykkauy@gmail.com>
2024-02-27 23:00:17 -0800

    array update

--- Day4-intro JS part 2/array.js
+++ Day4-intro JS part 2/array.js
@@ -2,3 +2,12 @@ let characters = ['goku', 'morgana','aang'];
 
 characters.push('sasuke');
 characters.pop();
+
+
+//slice : do not destroy original arrays
+let characters2 = characters.slice(1,3);
+console.log(characters2); // [ 'goku','morgana' ]
+
+// splice  destroy the org. array
+let newCharacters = characters.splice(1,2) ;
+console.log(newCharacterscharacters); //  
